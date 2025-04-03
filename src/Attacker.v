e015d9b6f64526dfc5fcd29c07a9f3fa8f7665b6
Tomas Castro Rojas <tomascastrorojas18@gmail.com>
2025-04-02 23:31:48 -0300

    seccion con propiedades de Machine

--- src/Attacker.v
+++ src/Attacker.v
@@ -9,16 +9,11 @@ Section muSE.
       secrets : list (idMachine * path); (* Conjunto de secretos obtenidos *)
       enviroment : idMachine -> option Machine (* Vista parcial *) (* Puede diferir entre atacantes *)
     }.
-    
-  Definition register_users (m:Machine) : idUser -> Prop := 
-    fun u => exists (l: list Account), (machine_accounts m) u = Some l.
-  Definition register_paths (m:Machine) : path -> Prop := 
-    fun p => exists (files: list path) (users: list idUser) (o: objective), (machine_fileSystem m) p = Some (files, users, o).
   
   Definition valid_attacker_i (a: Attacker) : Prop :=
     forall (m:idMachine) (u:idUser), (In (m,u) (known_machines a)) -> (exists (mac: Machine), (enviroment a) m = Some mac 
-                                                                                              /\ (register_users mac) u).
+                                                                                              /\ (registered_users mac) u).
   Definition valid_attacker_ii (a: Attacker) : Prop :=
     forall (m:idMachine) (p:path), (In (m,p) (secrets a)) -> (exists (mac: Machine), (enviroment a) m = Some mac 
-                                                                                     /\ (register_paths mac) p).                                                                     
+                                                                                     /\ (registered_paths mac) p).                                                                     
 End muSE.
--- src/Machine.v
+++ src/Machine.v
@@ -87,6 +87,54 @@ Section Machine.
       machine_neighbours : list idMachine;
     }.
     
+  (* Red de máquinas en un sistema *)
   Definition network_map := idMachine -> option Machine. 
   
 End Machine.
+
+Section Predicados.
+
+  Definition registered_users (m:Machine) (u: idUser) : Prop := 
+    exists (l: list Account), (machine_accounts m) u = Some l.
+    
+  Definition registered_paths (m:Machine) (p: path) : Prop := 
+    exists (files: list path) (users: list idUser) (o: objective), (machine_fileSystem m) p = Some (files, users, o).
+    
+  (* Todo usuario con acceso a un archivo es un usuario registrado en la maquina *) 
+  Definition users_access_to_files (m: Machine) : Prop :=
+    forall (u: idUser), 
+      (exists (p: path)
+              (files: list path)
+              (auth: list idUser)
+              (o: objective), (machine_fileSystem m) p = Some (files, auth, o) /\ (In u auth)) -> registered_users m u.
+              
+  (*
+  (* Para todo path en la maquina sus subarchivos estan registrados en la maquina *)
+  (* Para todo path que es un directorio, los archivos que contiene estan registrados en la maquina *)
+  Definition subfiles_in_machine (m: Machine) : Prop :=
+    forall (p: path),
+      (exists (files: list path)
+              (auth: list idUser)
+              (o: objective), (machine_fileSystem m) p = Some (files, auth, o)) -> (forall (p':path), (In p' files) -> registered m p').
+  *)
+  Definition is_neighbour (m: Machine) (mid: idMachine) :=
+    In mid (machine_neighbours m).
+    
+  (* Para todo servicio asociado a una cuenta, es un servicio registrado en la maquina *)
+  Definition users_access_to_services (m: Machine) : Prop :=
+    forall (s: idService),
+      (exists (u: idUser)
+              (l: list Account)
+              (acc: Account), (machine_accounts m) u = Some l 
+                              /\ (In acc l) 
+                              /\ (account_service acc) = s) -> (exists (serv: Service), (machine_services m) s = Some serv).
+  
+  (*
+  (* Para toda maquina que pertenece a la red, todos sus vecinos tambien pertenecen a la red y ella es vecina de ellos *)
+  Definition network_topology (network: network_map) : Prop :=
+    forall (mid: idMachine),
+      (exists (m: Machine), network mid = Some m) -> 
+        forall (neighbour: idMachine), is_neighbour m neighbour 
+          -> (exists (m':Machine), network neighbour = Some m' /\ is_neighbour m' mid).
+  *)
+End Predicados.
