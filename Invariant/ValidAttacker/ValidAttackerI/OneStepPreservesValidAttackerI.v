be39b972d6c9dec5d5d48ae54f2c25b171c0561b
Tomas Castro Rojas <tomascastrorojas18@gmail.com>
2025-10-01 17:48:24 -0300

    prueba del primer caso de valid_attacker_i

--- Invariant/ValidAttacker/ValidAttackerI/OneStepPreservesValidAttackerI.v
+++ Invariant/ValidAttacker/ValidAttackerI/OneStepPreservesValidAttackerI.v
@@ -11,8 +11,15 @@ Require Import Tesina.Technique.TechniqueOneStep.
 Require Import Tesina.Invariant.AuxLemmas.AuxLemmas.
 Require Import Tesina.Invariant.AuxTactics.
 
+Require Import Tesina.Invariant.ValidAttacker.ValidAttackerI.RemoteServices.
+
 Theorem one_step_preserves_valid_attacker_i : forall (a a' : Attacker) (t : Technique) (n: network_map),
       one_step a t n a' -> valid_attacker_i a' n.
   Proof.
-    admit.
+    intros a a' t n onestep.
+    destruct onestep.
+    induction t; unfold valid_attacker in H0; unfold valid_network in H;
+    unfold Pre in H1; unfold Post in H2; unfold valid_attacker_i; intros m' u' known_machines_a'.
+    - apply (one_step_remote_services_preserves_valid_attacker_i a a' network H0 i i1 i0 i2 k i3); auto.
+    
   Admitted.
--- Invariant/ValidAttacker/ValidAttackerI/RemoteServices.v
+++ Invariant/ValidAttacker/ValidAttackerI/RemoteServices.v
@@ -11,9 +11,36 @@ Require Import Tesina.Technique.TechniqueOneStep.
 Require Import Tesina.Invariant.AuxLemmas.AuxLemmas.
 Require Import Tesina.Invariant.AuxTactics.
 
-Lemma one_step_remote_services_preserves_valid_attacker_i : forall (m m': idMachine) (u u': idUser) (k: key) (s: idService) (a a' : Attacker) (n: network_map),
-      one_step a (Remote_Services m u m' u' k s) n a' -> valid_attacker_i a' n.
+Lemma one_step_remote_services_preserves_valid_attacker_i : forall (a a' : Attacker) (n: network_map) (aValid: valid_attacker a n) (m m': idMachine) (u u': idUser) (k': key) (s': idService),
+      valid_network n -> Pre a (Remote_Services m u m' u' k' s') -> Post a (Remote_Services m u m' u' k' s') n a' -> valid_attacker_i a' n.
   Proof.
-    intros.
-    admit.
-  Admitted.
+    intros a a' network validAttacker m m' u u' k' s' validNetwork pre post.
+    unfold valid_attacker_i.
+    intros m0 u0 known_machines_a'.
+    unfold valid_attacker in validAttacker.
+    elim validAttacker.
+    intros validAttackerI validAttackerII_III.
+    destruct pre; destruct post.
+    elim H2; intros secrets env; clear H2.
+    rewrite H1 in known_machines_a'.
+    rewrite env.
+    apply member_add_machine_user in known_machines_a'.
+    elim known_machines_a'.
+    - intro eq.
+      elim_intro_clear H0 H4 H0'.
+      elim_intro_clear H0' accs H0''.
+      elim_intro_clear H0'' acc H0'''.
+      elim_intro_clear H0''' mac' H6.
+      elim_intro_clear H6 env_mac' H7.
+      elim_intro_clear H7 accs_mac' H8.
+      exists mac'.
+      injection eq.
+      intros equ' eqm'.
+      split.
+      -- rewrite <- eqm'. exact env_mac'.
+      -- rewrite <- equ'. unfold registered_users. exists accs. exact accs_mac'.
+    - intro known_machines_a.
+      unfold valid_attacker_i in validAttackerI.
+      apply validAttackerI.
+      exact known_machines_a.
+  Qed.
